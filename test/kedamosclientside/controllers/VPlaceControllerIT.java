50e2c295120881e027d3a4a1b23da6c58f534c28
TARTANGALH\2dam <penemax572@gmail.com>
2022-02-03 10:55:30 +0100

    Test de place terminado

--- test/kedamosclientside/controllers/VPlaceControllerIT.java
+++ test/kedamosclientside/controllers/VPlaceControllerIT.java
@@ -92,6 +92,7 @@ public class VPlaceControllerIT extends ApplicationTest {
         FxToolkit.setupApplication(KedamosApp.class);
     }
 
+    //@Ignore
     @Test
     public void testA_enterWindow() {
         
@@ -101,13 +102,13 @@ public class VPlaceControllerIT extends ApplicationTest {
         write("abcd*1234");
         clickOn("#btnSignIn");
         
-        sleep(5000);
+        sleep(1000);
         
         clickOn("Events");
         sleep(500);
         clickOn("#miViewEvents");
         
-        sleep(5000);
+        sleep(1000);
         
         assertNotEquals("FAIL - Table has no data to click on",
                 lookup("#tvTable").queryTableView().getItems().size(), 0);
@@ -117,7 +118,7 @@ public class VPlaceControllerIT extends ApplicationTest {
         
         clickOn("#btnAddPlace");
         
-        sleep(5000);
+        sleep(1000);
         
         verifyThat("#placePane", isVisible());
         
@@ -211,7 +212,7 @@ public class VPlaceControllerIT extends ApplicationTest {
         verifyThat("#btnDelete", isEnabled());
         verifyThat("#btnCreate", isDisabled());
 
-        //Borrar todos los campos antes de ir a la segunda prueba
+        //Borrar todos los campos antes de ir a la siguiente prueba
         clickOn("#tfAddress");
         push(KeyCode.CONTROL, KeyCode.A);
         eraseText(1);
@@ -228,7 +229,7 @@ public class VPlaceControllerIT extends ApplicationTest {
 
     }
 
-    @Ignore
+    //@Ignore
     @Test
     public void testD_FieldsValidation() {
 
@@ -238,14 +239,57 @@ public class VPlaceControllerIT extends ApplicationTest {
         clickOn("#tfName");
         write("abcd*1234");
 
-        clickOn("<3tfPrice");
+        clickOn("#tfPrice");
         write("abcd*1234");
+        
+        clickOn("#dpDateRenewal");
+        push(KeyCode.F4);
+        push(KeyCode.DOWN);
+        push(KeyCode.ENTER);
 
         assertNotEquals("FAIL - Button is not enabled",
                 lookup("#btnCreate").query().isDisabled());
 
         clickOn("#btnCreate");
+        verifyThat("#lblAddressError", isVisible());
+        
+        clickOn("#tfAddress");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        write("aa");
+        
+        clickOn("#btnCreate");
+        verifyThat("#lblPriceError", isVisible());
+        
+        clickOn("#tfPrice");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        write("1");
+        
+        clickOn("#btnCreate");
+        verifyThat("#lblNameError", isVisible());
+        
+        clickOn("#tfName");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        write("aa");
+        
+        clickOn("#btnCreate");
+        verifyThat("#lblDateError", isVisible());
 
+        //Borrar todos los campos antes de ir a la segunda prueba
+        clickOn("#tfAddress");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfName");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfPrice");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        dpDateRenewal = lookup("#dpDateRenewal").query();
+        dpDateRenewal.setValue(null);
+        
     }
 
     //@Ignore
@@ -283,6 +327,19 @@ public class VPlaceControllerIT extends ApplicationTest {
         List<Place> places = table.getItems();
         assertEquals("FAIL - The place has not been added",
                 places.stream().filter(u -> u.getAddress().equals(address)).count(), 1);
+        
+        //Borrar todos los campos antes de ir a la segunda prueba
+        clickOn("#tfAddress");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfName");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfPrice");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        dpDateRenewal = lookup("#dpDateRenewal").query();
+        dpDateRenewal.setValue(null);
 
     }
 
@@ -309,6 +366,222 @@ public class VPlaceControllerIT extends ApplicationTest {
 
         assertEquals("FAIL - A row has been added", rowCount,
                 lookup("#table").queryTableView().getItems().size());
+        
+        //Borrar todos los campos antes de ir a la segunda prueba
+        clickOn("#tfAddress");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfName");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfPrice");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        dpDateRenewal = lookup("#dpDateRenewal").query();
+        dpDateRenewal.setValue(null);
+
+    }
+    
+    //@Ignore
+    @Test
+    public void testG_Modify() {
+
+        int rowCount = lookup("#table").queryTableView().getItems().size();
+        Place place = (Place) lookup("#table").queryTableView().getItems()
+                .get(rowCount-1);
+        String tableName = place.getName();
+        
+        assertNotEquals("FAIL - Table has no data to click on",
+                lookup("#table").queryTableView().getItems().size(), 0);
+        Node row = lookup(".table-row-cell").nth(rowCount-1).query();
+        assertNotNull("FAIL - Table has not that row. ", row);
+        clickOn(row);
+
+        clickOn("#tfName");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        write(tableName + "Modified");
+
+        sleep(500);
+
+        assertNotEquals("FAIL - Button is not enabled",
+                lookup("#btnModify").query().isDisabled());
+
+        sleep(500);
+
+        clickOn("#btnModify");
+
+        verifyThat("A place has been modified successfully", isVisible());
+
+        clickOn("Aceptar");
+        
+        table = lookup("#table").queryTableView();
+        
+        assertEquals("FAIL - A row has been added", rowCount,
+                lookup("#table").queryTableView().getItems().size());
+        
+        assertNotEquals("FAIL - The place was not modified", tableName, 
+                lookup("#table").queryTableView().getItems().get(0).getClass()
+                        .getName());
+        
+        //Borrar todos los campos antes de ir a la segunda prueba
+        clickOn("#tfAddress");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfName");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfPrice");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        dpDateRenewal = lookup("#dpDateRenewal").query();
+        dpDateRenewal.setValue(null);
+
+    }
+
+    //@Ignore
+    @Test
+    public void testH_ModifyExists() {
+
+        int rowCount = lookup("#table").queryTableView().getItems().size();
+        Place place = (Place) lookup("#table").queryTableView().getItems()
+                .get(1);
+        String tableAddress = place.getAddress();
+        String tableName = place.getName();
+        
+        
+        assertNotEquals("FAIL - Table has no data to click on",
+                lookup("#table").queryTableView().getItems().size(), 0);
+        Node row = lookup(".table-row-cell").nth(rowCount-2).query();
+        Node row2 = lookup(".table-row-cell").nth(1).query();
+        assertNotNull("FAIL - Table has not that row.", row);
+        assertNotNull("FAIL - Table has not that row.", row2);
+        clickOn(row);
+
+        clickOn("#tfAddress");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        write(tableAddress);
+
+        sleep(500);
+
+        assertNotEquals("FAIL - Button is not enabled",
+                lookup("#btnModify").query().isDisabled());
+
+        sleep(500);
+
+        clickOn("#btnModify");
+
+        verifyThat("#lblAddressExists", isVisible());
+        
+        table = lookup("#table").queryTableView();
+        
+        assertEquals("FAIL - A row has been added", rowCount,
+                lookup("#table").queryTableView().getItems().size());
+        
+        place = (Place) lookup("#table").queryTableView().getItems().get(1);
+        
+        assertEquals("FAIL - The place has been modified", tableName,
+                place.getName());
+        
+        //Borrar todos los campos antes de ir a la segunda prueba
+        clickOn("#tfAddress");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfName");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfPrice");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        dpDateRenewal = lookup("#dpDateRenewal").query();
+        dpDateRenewal.setValue(null);
+
+    }
+    
+    //@Ignore
+    @Test
+    public void testI_DeleteCancel() {
+
+        int rowCount = lookup("#table").queryTableView().getItems().size();        
+        
+        assertNotEquals("FAIL - Table has no data to click on",
+                lookup("#table").queryTableView().getItems().size(), 0);
+        Node row = lookup(".table-row-cell").nth(rowCount-2).query();
+        assertNotNull("FAIL - Table has not that row.", row);
+        clickOn(row);
+
+        assertNotEquals("FAIL - Button is not enabled",
+                lookup("#btnModify").query().isDisabled());
+
+        sleep(500);
+
+        clickOn("#btnDelete");
+
+        verifyThat("Are you sure you want to delete this Place?", isVisible());
+        
+        clickOn("Cancelar");
+        
+        table = lookup("#table").queryTableView();
+        
+        assertEquals("FAIL - The row has been removed", rowCount,
+                lookup("#table").queryTableView().getItems().size());
+        
+        //Borrar todos los campos antes de ir a la segunda prueba
+        clickOn("#tfAddress");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfName");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfPrice");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        dpDateRenewal = lookup("#dpDateRenewal").query();
+        dpDateRenewal.setValue(null);
+
+    }
+    
+    //@Ignore
+    @Test
+    public void testJ_Delete() {
+
+        int rowCount = lookup("#table").queryTableView().getItems().size();        
+        
+        assertNotEquals("FAIL - Table has no data to click on",
+                lookup("#table").queryTableView().getItems().size(), 0);
+        Node row = lookup(".table-row-cell").nth(rowCount-2).query();
+        assertNotNull("FAIL - Table has not that row.", row);
+        clickOn(row);
+
+        assertNotEquals("FAIL - Button is not enabled",
+                lookup("#btnModify").query().isDisabled());
+
+        sleep(500);
+
+        clickOn("#btnDelete");
+
+        verifyThat("Are you sure you want to delete this Place?", isVisible());
+        
+        clickOn("Aceptar");
+        
+        table = lookup("#table").queryTableView();
+        
+        assertNotEquals("FAIL - The row has not been removed", rowCount,
+                lookup("#table").queryTableView().getItems().size());
+        
+        //Borrar todos los campos antes de ir a la segunda prueba
+        clickOn("#tfAddress");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfName");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        clickOn("#tfPrice");
+        push(KeyCode.CONTROL, KeyCode.A);
+        eraseText(1);
+        dpDateRenewal = lookup("#dpDateRenewal").query();
+        dpDateRenewal.setValue(null);
 
     }
 
